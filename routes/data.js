04b0ee9278aefc962627fe4c3b8db311afacb00a
Rauno Viskus <rauno56@gmail.com>
2016-10-23 11:54:11 +0300

    implement getComparison endpoint

--- routes/data.js
+++ routes/data.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const api = require('../lib/dataApi');
+const _ = require('lodash');
 
 router.get('/getStats', function (req, res, next) {
 	const fee = parseFloat(req.query.fee) || undefined;
 	if (req.query.isin) {
 		return api.getStats(req.query.isin, fee)
+			.then(result => res.send(result))
+			.catch(next);
+	}
+
+	throw new Error('Required parameter missing: "isin".');
+});
+
+router.get('/getComparison', function (req, res, next) {
+	const fee = parseFloat(req.query.fee) || undefined;
+	let indexRatio = parseFloat(req.query.indexRatio);
+	if (!_.isFinite(indexRatio)) indexRatio = 0.5;
+
+	if (req.query.isin) {
+		return api.getComparison(req.query.isin, indexRatio, fee)
 			.then(result => res.send(result))
 			.catch(next);
 	}
