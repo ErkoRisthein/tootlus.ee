5bd498b3713064d8732da2329389b5a70fe64256
Rauno Viskus <rauno56@gmail.com>
2016-10-23 12:21:17 +0300

    check if isin is provided as the first thing

--- routes/data.js
+++ routes/data.js
@@ -4,28 +4,29 @@ const api = require('../lib/dataApi');
 const _ = require('lodash');
 
 router.get('/getStats', function (req, res, next) {
-	const fee = parseFloat(req.query.fee) || undefined;
-	if (req.query.isin) {
-		return api.getStats(req.query.isin, fee)
-			.then(result => res.send(result))
-			.catch(next);
+	if (!req.query.isin) {
+		throw new Error('Required parameter missing: "isin".');
 	}
 
-	throw new Error('Required parameter missing: "isin".');
+	const fee = parseFloat(req.query.fee) || undefined;
+
+	return api.getStats(req.query.isin, fee)
+		.then(result => res.send(result))
+		.catch(next);
 });
 
 router.get('/getComparison', function (req, res, next) {
+	if (!req.query.isin) {
+		throw new Error('Required parameter missing: "isin".');
+	}
+
 	const fee = parseFloat(req.query.fee) || undefined;
 	let indexRatio = parseFloat(req.query.indexRatio);
 	if (!_.isFinite(indexRatio)) indexRatio = 0.5;
 
-	if (req.query.isin) {
-		return api.getComparison(req.query.isin, indexRatio, fee)
-			.then(result => res.send(result))
-			.catch(next);
-	}
-
-	throw new Error('Required parameter missing: "isin".');
+	return api.getComparison(req.query.isin, indexRatio, fee)
+		.then(result => res.send(result))
+		.catch(next);
 });
 
 router.use('/', (err, req, res, next) => {
