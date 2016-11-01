8ff1418a758e2bef0e90f3fc11baa9b9d0bf58af
Rauno Viskus <rauno56@gmail.com>
2016-11-01 21:38:12 +0200

    make port configurable

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const config = require('./config');
+
 const express = require('express');
 const app = express();
 
 app.use('/', express.static(__dirname + '/public/'));
 app.use('/data', require('./routes/data'));
 
-app.listen(80, function () {
+app.listen(config.port || 1337, function () {
   console.log('Express listening on port', this.address().port);
 });
